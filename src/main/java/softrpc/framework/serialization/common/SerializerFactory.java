b4a581f1c66da3e7968a9be6710fbd117948964e
xcNew <476336251@qq.com>
2020-02-16 14:00:46 +0800

    序列化工厂类优化

--- src/main/java/softrpc/framework/serialization/common/SerializerFactory.java
+++ src/main/java/softrpc/framework/serialization/common/SerializerFactory.java
@@ -5,6 +5,9 @@ import softrpc.framework.serialization.serializer.JDKSerializer;
 import softrpc.framework.serialization.serializer.ProtoStuffSerializer;
 import softrpc.framework.serialization.serializer.Serializer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Serializer简单工厂，提供根据序列化协议名称获得Serializer对象实例的功能
  *
@@ -12,12 +15,21 @@ import softrpc.framework.serialization.serializer.Serializer;
  * @date 2019/11/10
  */
 public class SerializerFactory {
+
+    static  Map<String,Serializer> SERIALIZER_MAP = new HashMap<>();
+
+    static {
+        SERIALIZER_MAP.put("hessian",new HessianSerializer());
+        SERIALIZER_MAP.put("protostuff",new ProtoStuffSerializer());
+        SERIALIZER_MAP.put("default",new JDKSerializer());
+    }
     public static Serializer getSerializer(String serializerName){
         String validName = serializerName.toLowerCase();
-        switch (validName){
-            case "hessian": return new HessianSerializer();
-            case "protostuff": return new ProtoStuffSerializer();
-            default: return  new JDKSerializer();
+        Serializer serializer = SERIALIZER_MAP.get(validName);
+        if(serializer != null){
+            return serializer;
+        }else {
+            return SERIALIZER_MAP.get("default");
         }
     }
 }
